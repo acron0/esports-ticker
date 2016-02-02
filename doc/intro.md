877de05b0d932def0f627c00f19763f2f03895d6
Antony Woods <antony@mastodonc.com>
2016-02-02 21:26:57 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to esports-ticker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/esports_ticker/core_test.clj
@@ -0,0 +1,7 @@
+(ns esports-ticker.core-test
+  (:require [clojure.test :refer :all]
+            [esports-ticker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
